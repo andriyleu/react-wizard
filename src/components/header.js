21d9ee3a21b30a2464cb6a2100ad23d99ffe0e1a
Andriy Leutskyy Polishchuk <andriy.leutskyy@vipera.com>
2021-02-03 16:42:39 +0100

    feat: added bigger size state and centered text properly for all cases

--- src/components/header.js
+++ src/components/header.js
@@ -17,28 +17,34 @@ const AppNav = styled.nav`
 const NavList = styled.ul`
   position: relative;
   padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const CircleNavElement = styled.li`
-  width: 2em;
-  height: 2em;
+  width: ${(props) => (props.isActive ? "3em" : "2em")};
+  height: ${(props) => (props.isActive ? "3em" : "2em")};
   text-align: center;
   line-height: 2em;
-  border-radius: 1em;
-  background: #002B45;
+  border-radius: 3em;
+  background: #002b45;
   margin: 0 1em;
-  display: inline-block;
   color: white;
   position: relative;
+
+  display: flex;
+  align-items: center; 
+  justify-content: center;
 
   &:before {
     content: "";
     position: absolute;
-    top: 0.9em;
+    top: 50%;
     left: -2em;
     width: 2em;
     height: 0.2em;
-    background: #788F9C;
+    background: ${(props) => (props.isPending ? "#788f9c" : "red")};
   }
 
   &:first-child:before {
@@ -47,16 +53,33 @@ const CircleNavElement = styled.li`
 `;
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentStep: 0 };
+  }
+
   render() {
+    const circleElements = Array.from(
+      { length: this.props.numberOfSteps },
+      (_, i) => i + 1
+    ).map((i) => {
+      console.log("currentstep", this.props.currentStep);
+      console.log("i", i);
+      return (
+        <CircleNavElement
+          isActive={this.props.currentStep === i}
+          isPending={i > this.props.currentStep}
+        >
+          {i}
+        </CircleNavElement>
+      );
+    });
+
     return (
       <>
         <AppHeader>
           <AppNav>
-            <NavList>
-              <CircleNavElement>1</CircleNavElement>
-              <CircleNavElement>2</CircleNavElement>
-              <CircleNavElement>3</CircleNavElement>
-            </NavList>
+            <NavList>{circleElements}</NavList>
           </AppNav>
         </AppHeader>
       </>
