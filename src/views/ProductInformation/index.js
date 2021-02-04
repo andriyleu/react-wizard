37f866583d0abb06bd520551cf552d85f764ecd4
Andriy Leutskyy Polishchuk <andriy.leutskyy@vipera.com>
2021-02-04 16:21:43 +0100

    refactor: some small html changes, branch ready to merge

--- src/views/ProductInformation/index.js
+++ src/views/ProductInformation/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import brain from "../../assets/img/group.svg";
 import safe from "../../assets/img/group-3.svg";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import PageTitle from "../../components/page-title";
-import ImageWithDescription from "../../components/imagewithdescription";
+import ImageWithDescription from "../../components/image-with-description";
 
 const ImagesSection = styled.section`
   display: flex;
@@ -32,20 +32,24 @@ class Step1 extends Component {
             description="Crea tu clave maestra: solo tú podrás acceder o tus secretos con ella."
           ></ImageWithDescription>
         </ImagesSection>
-        <div className="page_content">
+        <section>
           <div>
             <SubTitle>Cómo funciona</SubTitle>
-            <p>En primer lugar, debes crear una contraseña diferente para sus
-            pertenencias electrónicos. No podrás recuperar tu contraseña, así
-            que recuérdela bien.</p>
+            <p>
+              En primer lugar, debes crear una contraseña diferente para sus
+              pertenencias electrónicos. No podrás recuperar tu contraseña, así
+              que recuérdela bien.
+            </p>
           </div>
           <div>
             <SubTitle>Qué datos puedes guardar</SubTitle>
-            <p>Por ejemplo, el número de tu tarjeta, el PIN y el PLIK de tu
-            teléfono móvil, el número de serie de alguno de tus dispositivos o
-            cualquier información que necesites tener en lugar seguro.</p>
+            <p>
+              Por ejemplo, el número de tu tarjeta, el PIN y el PLIK de tu
+              teléfono móvil, el número de serie de alguno de tus dispositivos o
+              cualquier información que necesites tener en lugar seguro.
+            </p>
           </div>
-        </div>
+        </section>
       </>
     );
   }
